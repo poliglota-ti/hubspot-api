f325892100f02703587fad4360f2687374557f5b
Dinko <rocket4ce@gmail.com>
2022-11-24 12:34:14 -0300

    fix: association contact deal

--- lib/hubspot/urls.rb
+++ lib/hubspot/urls.rb
@@ -4,7 +4,7 @@ module Hubspot
             base_v3: 'https://api.hubapi.com/crm/v3/objects/',
             search_contact: 'contacts/search',
             contact: 'contacts',
-            deals: 'deals'
-            association_contact_deal: 'contacts/contactId/associations/toObjectType/toObjectId'
+            deals: 'deals',
+            association_contact_deal: 'contacts/contactId/associations/toObjectType/toObjectId/contact_to_deal'
           }.freeze
 end
