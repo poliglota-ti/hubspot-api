4be053b97f6de5d5eae0f5989201aa4f4ad1b9b1
Dinko <rocket4ce@gmail.com>
2022-11-28 16:07:38 -0300

    add: create property and add get deal by id and search_property_by_name

--- lib/hubspot/build_request.rb
+++ lib/hubspot/build_request.rb
@@ -11,7 +11,11 @@ module Hubspot
       post(url, params)
     end
 
-
+    def self.get_deal_by_id(id)
+      raise "id cant be empty or nil" if id.nil?
+      url = base_v3_url+deals_url+"/#{id}"
+      get(url)
+    end
 
     def self.create_user(params={})
       raise "params cant be empty or nil" if params.empty? || params.nil?
@@ -35,11 +39,27 @@ module Hubspot
 
     def self.create_association(params={})
       raise "params cant be empty or nil" if params.empty? || params.nil?
-      url = base_v3_url+association_url(params)
+      url = base_v3_url+set_url(params, "association_contact_deal")
       update(url)
     end
-    
+
+    def self.search_property_by_name(params)
+      raise "params cant be empty or nil" if params.empty? || params.nil?
+      url = base_v3_url+set_url(params, "properties")
+      get(url)
+    end
+
+    def self.create_property(params)
+      raise "params cant be empty or nil" if params.empty? || params.nil?
+      url = base_v3_url+property_url
+      post(url, params)
+    end
+      
     private
+
+      def self.get(url)
+        HTTParty.get(url, headers: set_header)
+      end
 
       def self.patch(url, params)
         HTTParty.patch(url, body: params.to_json, headers: set_header)
@@ -70,8 +90,12 @@ module Hubspot
       def self.deals_url
         Hubspot::URLS[:deals]
       end
-      def self.association_url(params)
-        arr_url = Hubspot::URLS[:association_contact_deal].split("/")
+      def self.property_url
+        Hubspot::URLS[:property]
+      end
+
+      def self.set_url(params, set_url)
+        arr_url = Hubspot::URLS[set_url.to_sym].split("/")
 
         new_url = arr_url.map do |word|
           params[word.to_sym].nil? ? word : params[word.to_sym]
