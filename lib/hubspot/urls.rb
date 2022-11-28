c668666b4e098ca446a169828b4fd318f508237a
Dinko <rocket4ce@gmail.com>
2022-11-28 16:07:53 -0300

    fix: urls

--- lib/hubspot/urls.rb
+++ lib/hubspot/urls.rb
@@ -1,10 +1,12 @@
 module Hubspot
   URLS =  {
-            base_v4: 'https://api.hubapi.com/crm/v4/objects/',
-            base_v3: 'https://api.hubapi.com/crm/v3/objects/',
-            search_contact: 'contacts/search',
-            contact: 'contacts',
-            deals: 'deals',
-            association_contact_deal: 'contacts/contactId/associations/toObjectType/toObjectId/contact_to_deal'
+            base_v4: 'https://api.hubapi.com/crm/v4/',
+            base_v3: 'https://api.hubapi.com/crm/v3/',
+            search_contact: 'objects/contacts/search',
+            contact: 'objects/contacts',
+            deals: 'objects/deals',
+            association_contact_deal: 'objects/contacts/contactId/associations/toObjectType/toObjectId/contact_to_deal',
+            properties: 'properties/objectType/propertyName',
+            property:'properties/Deal'
           }.freeze
 end
